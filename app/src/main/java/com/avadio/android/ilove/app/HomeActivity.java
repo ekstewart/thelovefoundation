ca90627630fd35b5c52f3b6855418852930be40c
changjieyang <changjieyang@gmail.com>
2014-10-29 17:53:05 -0700

    Updated background image, logo, sharedactionprovider, and random quote selection.

--- app/src/main/java/com/avadio/android/ilove/app/HomeActivity.java
+++ app/src/main/java/com/avadio/android/ilove/app/HomeActivity.java
@@ -1,20 +1,13 @@
 package com.avadio.android.ilove.app;
 
-import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.support.v4.content.Loader;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
@@ -23,17 +16,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Random;
 
 import database.MyContentProvider;
@@ -47,7 +33,7 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
     private TextView mQuoteText;
     private ShareActionProvider mShareActionProvider;
 
-    Animation mAnimation;
+//    Animation mAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,15 +44,17 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
-        }
-        getSupportLoaderManager().initLoader(0, null, HomeActivity.this);
 
-        //TODO:  Don't change quote on back
-        // Run background task to load data into SQLite Database
-//        LoadQuotesTask loadQuotesTask = new LoadQuotesTask();
-//        loadQuotesTask.execute();
+            //TODO:  Don't change quote on back
+            getSupportLoaderManager().initLoader(0, null, HomeActivity.this);
+        }
     }
 
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        outState.putString("quote", mQuoteText.getText().toString());
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -91,7 +79,7 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
     private Intent getDefaultIntent() {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/*");
-        intent.putExtra(Intent.EXTRA_TEXT, mQuoteText.getText() == null ? "" : mQuoteText.getText());
+        intent.putExtra(Intent.EXTRA_TEXT, mQuoteText == null ? "Temp" : mQuoteText.getText() + "\nBy: Harold B. Becker");
 //        intent.putExtra(Intent.EXTRA_TEXT, "Test shareIntent");
         //TODO:  format shared quote
 
@@ -116,24 +104,24 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
         String[] projection = {QUOTESTABLE.COL_ID, QUOTESTABLE.COL_QUOTE};
-        CursorLoader loader = new CursorLoader(
+        return new CursorLoader(
                 this,
                 MyContentProvider.CONTENT_URI,
                 projection,
                 null,
                 null,
                 null);
-        return loader;
     }
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        Log.d(TAG, "Number of quotes loaded = " + data.getCount());
         mCursor = data;
 
         // Read from cursor
         if (mCursor != null && mCursor.getCount() > 0) {
 //            mCursor.moveToFirst();
-            // Move to random row
+            // Move cursor to a random position
             int row = randInt(0, mCursor.getCount());
             mCursor.moveToPosition(row);
 
@@ -153,7 +141,10 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
 
     public void OnTextClick(View view) {
         if (mCursor != null && mCursor.getCount() > 0) {
-            mCursor.moveToNext();
+//            mCursor.moveToNext();
+            // Move cursor to a random position
+            int row = randInt(0, mCursor.getCount() -1);
+            mCursor.moveToPosition(row);
             int idIndex = mCursor.getColumnIndex(QUOTESTABLE.COL_QUOTE);
 
             String quote = mCursor.getString(idIndex);
@@ -181,67 +172,66 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-            return rootView;
+            return inflater.inflate(R.layout.fragment_home, container, false);
         }
     }
 
     /* Use asyn task to load entries to the sqlite database */
-    private class LoadQuotesTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        public Void doInBackground(Void... params) {
-
-            try {
-                loadQuotes();
-            } catch (IOException e) {
-                Log.e(TAG, e.getMessage());
-            }
-            return null;
-        }
-
-        @Override
-        public void onPostExecute(Void param) {
-            super.onPostExecute(param);
-
-            // Update UI
-            getSupportLoaderManager().initLoader(0, null, HomeActivity.this);
-        }
-    }
+//    private class LoadQuotesTask extends AsyncTask<Void, Void, Void> {
+//        @Override
+//        public Void doInBackground(Void... params) {
+//
+//            try {
+//                loadQuotes();
+//            } catch (IOException e) {
+//                Log.e(TAG, e.getMessage());
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        public void onPostExecute(Void param) {
+//            super.onPostExecute(param);
+//
+//            // Update UI
+//            getSupportLoaderManager().initLoader(0, null, HomeActivity.this);
+//        }
+//    }
 
 
     /* Load quots from raw text file */
-    private void loadQuotes() throws IOException {
-        final Resources resources = getResources();
-        InputStream inputStream = resources.openRawResource(R.raw.quotes);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-
-        try {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                //String[] strings = TextUtils.sp
-
-                Uri uri;
-                if (!line.equals("") && !line.startsWith("Quotes by")) {
-                    uri = addQuote(line.trim());
-                    if (uri.getLastPathSegment().equals("")) {
-                        Log.e(TAG, "unable to add quote: " + line.trim());
-                    }
-                }
-            }
-        } finally {
-            reader.close();
-        }
-
-    }
+//    private void loadQuotes() throws IOException {
+//        final Resources resources = getResources();
+//        InputStream inputStream = resources.openRawResource(R.raw.quotes);
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+//
+//        try {
+//            String line;
+//            while ((line = reader.readLine()) != null) {
+//                //String[] strings = TextUtils.sp
+//
+//                Uri uri;
+//                if (!line.equals("") && !line.startsWith("Quotes by")) {
+//                    uri = addQuote(line.trim());
+//                    if (uri.getLastPathSegment().equals("")) {
+//                        Log.e(TAG, "unable to add quote: " + line.trim());
+//                    }
+//                }
+//            }
+//        } finally {
+//            reader.close();
+//        }
+//
+//    }
 
     /* Insert quote to SQLite database */
-    public Uri addQuote(String quote) {
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("quote", quote);
-        contentValues.put("author", "Harold B. Becker");
-
-        return getContentResolver().insert(MyContentProvider.CONTENT_URI, contentValues);
-    }
+//    public Uri addQuote(String quote) {
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put("quote", quote);
+//        contentValues.put("author", "Harold B. Becker");
+//
+//        return getContentResolver().insert(MyContentProvider.CONTENT_URI, contentValues);
+//    }
 
     /**
      * Returns a pseudo-random number between min and max, inclusive.
@@ -254,15 +244,12 @@ public class HomeActivity extends ActionBarActivity implements LoaderCallbacks<C
      * @see java.util.Random#nextInt(int)
      */
     public static int randInt(int min, int max) {
-
         // NOTE: Usually this should be a field rather than a method
         // variable so that it is not re-seeded every call.
         Random rand = new Random();
 
         // nextInt is normally exclusive of the top value,
         // so add 1 to make it inclusive
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-
-        return randomNum;
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
